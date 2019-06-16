b09e46258483be7717291357083a758562490d92
hongge <1361255790.@qq.com>
2019-06-16 18:43:49 +0800

    add maven

--- order-common/src/main/java/com/griftt/common/feign/GoodsFeign.java
+++ order-common/src/main/java/com/griftt/common/feign/GoodsFeign.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name = "eureka-product")
+@FeignClient(name = "product")
 public interface GoodsFeign {
 
     @GetMapping("/goods/all")
--- order-server/src/main/java/com/griftt/orderserver/OrderServerApplication.java
+++ order-server/src/main/java/com/griftt/orderserver/OrderServerApplication.java
@@ -2,12 +2,18 @@ package com.griftt.orderserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@ComponentScan(basePackages ="com.griftt.common")
+@EnableJpaRepositories("com.griftt.common.repository")
+@EntityScan("com.griftt.common.entity")
 public class OrderServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServerApplication.class, args);

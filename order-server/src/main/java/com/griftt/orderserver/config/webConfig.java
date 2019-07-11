2dc807048e9f5b9c968bbd6067fa1b50778ed110
griftt <1361255708@qq.com>
2019-07-11 18:27:07 +0800

    add 链路追踪

--- order-server/src/main/java/com/griftt/orderserver/config/webConfig.java
+++ order-server/src/main/java/com/griftt/orderserver/config/webConfig.java
@@ -2,17 +2,7 @@ package com.griftt.orderserver.config;
 
 import com.griftt.orderserver.interceptor.BaseControllerInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.stereotype.Component;
-import org.springframework.validation.MessageCodesResolver;
-import org.springframework.validation.Validator;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.*;
-
-import java.util.List;
 
 /**
  * 项目的各种大配置 如拦截器
